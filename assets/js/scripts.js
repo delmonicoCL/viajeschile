10b786d3795a5817d2325853e0ea276618fd91a0
delmonicoCL <taiker17@gmail.com>
2021-05-23 22:42:46 -0400

    Ultimos cambios, se eliminio el llamdo del popper

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
     $("a").click(function(event) {
         event.preventDefault();
-
         var cambio = this.hash;
-
         $("html").animate(
             {
                 scrollTop: $(cambio).offset().top,
